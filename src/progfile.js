1ccac16b8fd74b5f40c3702ec4045c1a4f564458
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-06-22 12:36:29 -0400

    Add comments

--- src/progfile.js
+++ src/progfile.js
@@ -13,8 +13,9 @@ const fs = require("fs");
 function readProgFile(file = "main.sfth") {
   // Read program file
   // Convert newlines to spaces, removing empty lines
-  const prog = fs.readFileSync(file).toString().split("\n").filter(n=>n.length>0).join(" ");
-  
+  const prog = fs.readFileSync(file).toString().split("\n").filter(n =>
+    n.length > 0 && !n.startsWith("#")).join(" ");
+
   return prog;
 }
 
