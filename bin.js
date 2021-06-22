756df5184ac39299aba82263f9ee2eb41ba1a813
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-06-22 12:04:52 -0400

    Update bin.js

--- bin.js
+++ bin.js
@@ -1,3 +1,13 @@
+/*********************************************
+ *   ScratchForth - Forth implementation for *
+ *          Scratch Binary Format            *
+ *                                           *
+ *   Copyright (C) 2021  AmazingMech2418     *
+ *                                           *
+ *   Licensed under GPLv3 - see license in   *
+ *            the LICENSE file               *
+ *********************************************/
+
 const process = require("process");
 
 require(".")(process.argv);
