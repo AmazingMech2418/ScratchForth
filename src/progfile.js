eb6a6de1cd4bfbe31079c20c0e4b2b91e96cfd75
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-06-22 13:45:08 -0400

    Parse compiler instructions on file read

--- src/progfile.js
+++ src/progfile.js
@@ -10,13 +10,22 @@
 
 const fs = require("fs");
 
-function readProgFile(file = "main.sfth") {
+function readProgFile(file = "main.sfth", shouldJoin = true) {
   // Read program file
   // Convert newlines to spaces, removing empty lines
   const prog = fs.readFileSync(file).toString().split("\n").filter(n =>
-    n.length > 0 && !n.startsWith("#")).join(" ");
+    n.length > 0 && !n.startsWith("#"));
 
-  return prog;
+  for(let i = 0; i < prog.length; i++) {
+    if(prog[i].startsWith("$")) {
+      const compilerInstructions = require("./compilerinstructions.js");
+
+      let newStuff = compilerInstructions(prog[i].slice(1).trim(), prog, file);
+      prog.splice(i, 1, ...newStuff);
+    }
+  }
+
+  return shouldJoin?prog.join(" "):prog;
 }
 
 module.exports = readProgFile;
