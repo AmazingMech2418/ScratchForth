1a3228469c1b1f4a00b605ebe483a6b5caa216ea
AmazingMech2418 <33498997+AmazingMech2418@users.noreply.github.com>
2021-06-22 10:27:31 -0400

    Update charlist.js

--- src/charlist.js
+++ src/charlist.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 function getCharList() {
-  fs.readFileSync("../resources/CHARS.txt").toString().split("\r").join("").split("\n");
+  fs.readFileSync("./resources/CHARS.txt").toString().split("\r").join("").split("\n");
 }
 
 module.exports = getCharList;
